0a1d8d7c3457f3a6eb689a7ed8038c5d89db526d
Chanon-SVN <chanon@kmi.tl>
2014-09-20 19:09:09 +0700

    add product

---
+++ application/views/pages/product.php
@@ -0,0 +1,19 @@
+﻿
+
+<section style="background-color:#fff; padding-bottom:5%;">
+  <div class="container">
+    <div class="row">
+   <span style="font-size:200%;">Webserv 3.0</span>
+		<div class="row">
+		  <div class="col-md-4">
+			<a href="#" class="thumbnail">
+			  <img data-src="holder.js/100%x180"src="<?= base_url('/assets/images/webserv.png') ?>" alt="...">
+			</a>
+		  </div>
+		  <div class="col-md-7 col-md-offset-1">
+			<span>WebServ.KMITL เป็นบริการฟรีพื้นที่ให้บริการเว็บไซต์ (Free hosting) โดยรองรับการประมวลผลโดยใช้ภาษา PHP และรองรับฐานข้อมูล MySQL5 ซึ่งเปิดบริการเฉพาะนักศึกษา, อาจารย์, บุคลากรภายในสถาบันเท่านั้น ทั้งนี้บริการ WebServ.KMITL ได้เปิดให้บริการตั้งแต่เดือนกันยายน พ.ศ. 2545 และมีสถิติการใช้งานอย่างต่อเนื่อง.</span>
+		  </div>
+		</div>		 
+    </div>
+  </div>
+</section>
