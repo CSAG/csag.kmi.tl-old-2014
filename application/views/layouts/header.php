d39fef7cc7d976da50cdfceb4153bf6656a18094
n1ghthawk <n.thanaphon.soo@gmail.com>
2014-09-20 23:40:03 +0700

    add modal

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -45,7 +45,7 @@
                 </ul>
               </li>
             </ul>
-            <? if (!$logged_in): ?>
+            <?php if (!$logged_in): ?>
               <?= form_open('sessions/create', array('class' => 'navbar-form navbar-right')); ?>
                 <div class="form-group">
                   <input type="text" class="form-control" name="email" placeholder="Email">
@@ -55,13 +55,13 @@
                 </div>
                 <button type="submit" class="btn btn-default">Login</button>
               </form>
-            <? else: ?>
+            <?php else: ?>
               <ul class="nav navbar-nav navbar-right">
                 <li><p class="navbar-text navbar-right">Welcome <?= $this->session->userdata('fullname') ?></p></li>
                 <li class="divider-vertical"></li>
                 <li><a href="<?= site_url('sessions/destroy') ?>">Log out</a></li>
               </ul>
-            <? endif; ?>
+            <?php endif; ?>
           </div>
         </div>
       </nav>
